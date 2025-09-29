6f64aafed528a1e16a5adac7c13efd57d5cb7fb6
Angel Guillermo Bosquez Baltazar <A01667100@tec.mx>
2025-09-29 12:29:24 -0600

    Created PrimaryButtonComponent.swift

---
+++ componentesSwift/PrimaryButtonComponent.swift
@@ -0,0 +1,36 @@
+//
+//  PrimaryButtonComponent.swift
+//  RepNet
+//
+//  Created by Angel Bosquez on 28/09/25.
+//
+
+
+import SwiftUI
+
+struct PrimaryButtonComponent: View {
+    let title: String
+    let action: () -> Void
+    var isEnabled: Bool = true
+
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.buttonFont)
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(isEnabled ? Color.primaryBlue : Color.disabledGray)
+                .cornerRadius(12)
+        }
+        .disabled(!isEnabled)
+    }
+}
+//Preview creado con gemini
+#Preview {
+    VStack(spacing: 20) {
+        PrimaryButtonComponent(title: "Botón Activado", action: {}, isEnabled: true)
+        PrimaryButtonComponent(title: "Botón Desactivado", action: {}, isEnabled: false)
+    }
+    .padding()
+}
